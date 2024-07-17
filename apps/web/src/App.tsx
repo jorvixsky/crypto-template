2adf407d9e1358912be7f367a0126038e0524c5a
jorvixsky <jorvixsky@proton.me>
2024-07-17 23:59:25 +0200

    Implemented counter with contract (pending auto refresh)

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,10 +1,28 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import contract from '../contracts.json'
+import { ConnectButton } from '@rainbow-me/rainbowkit'
+import { useReadContract, useWriteContract } from 'wagmi'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const counter = useReadContract({
+    address: contract[0].address as `0x${string}`,
+    abi: contract[0].abi,
+    functionName: 'count',
+  })
+
+  const { writeContract } = useWriteContract()
+
+  const increment = () => {
+    writeContract({
+      address: contract[0].address as `0x${string}`,
+      abi: contract[0].abi,
+      functionName: 'increment',
+    })
+  }
+
+  const count = counter.data ? counter.data.toString() : '0'
 
   return (
     <>
@@ -17,8 +35,11 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <ConnectButton />
+      </div>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={increment}>
           count is {count}
         </button>
         <p>
--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,22 +1,31 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { WagmiProvider } from 'wagmi'
-import { wagmiConfig } from './wagmi'
-import App from './App.tsx'
-import './index.css'
-import { RouterProvider, createHashRouter } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { WagmiProvider } from "wagmi";
+import { wagmiConfig } from "./wagmi";
+import App from "./App.tsx";
+import "@rainbow-me/rainbowkit/styles.css";
+import "./index.css";
+import { RouterProvider, createHashRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
 
 const router = createHashRouter([
   {
-    path: '/',
+    path: "/",
     element: <App />,
   },
-])
+]);
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <WagmiProvider config={wagmiConfig}>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>
+          <RouterProvider router={router} />
+        </RainbowKitProvider>
+      </QueryClientProvider>
     </WagmiProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
