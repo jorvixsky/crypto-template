721b18c993fda392ec97a94d2379cd23a21ede94
jorvixsky <jorvixsky@proton.me>
2024-07-17 23:33:21 +0200

    Installed react router & configured wagmi

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { WagmiProvider } from 'wagmi'
+import { wagmiConfig } from './wagmi'
 import App from './App.tsx'
 import './index.css'
+import { RouterProvider, createHashRouter } from 'react-router-dom'
+
+const router = createHashRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <WagmiProvider config={wagmiConfig}>
+      <RouterProvider router={router} />
+    </WagmiProvider>
   </React.StrictMode>,
 )
